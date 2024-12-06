72474b9dd1002177e436cd61984fe451ee8ff1d0
Raon98 <cheol1998@kakao.com>
2024-12-06 13:24:46 +0900

    feat : h2 db 연결 테스트

---
+++ src/main/java/com/restful_spring/entity/user/UserEntity.java
@@ -0,0 +1,29 @@
+package com.restful_spring.entity.user;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 30)
+    private String name;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+
+}
